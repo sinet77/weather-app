235c90cf804e3f68aef9753d12f8f2474ce4b3e8
Paweł Kozłowski <152084345+sinet77@users.noreply.github.com>
2024-03-28 08:57:18 +0100

    dynamic html

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
 const searchButton = document.getElementById('button');
 const cityInput = document.getElementById('bar');
+const app = document.querySelector('.white-board')
 
 // if(!city){
 //     alert ("Please enter the city");
@@ -27,7 +28,36 @@ searchButton.addEventListener('click', async () => {
     const cityName = cityInput.value;
     const weatherObject = await fetchWeatherData(cityName)
     const description = weatherObject.weather[0].description
-    console.log(description)
+
+
+    console.log(weatherObject) //trzeba zobaczyc wszystkie wlasciwosci, np rodzaje pogody
 })
 
+function createHtml(responseData){
 
+    const bar = document.createElement('div');
+    bar.classList.add('bar');   //bar ma id
+    app.appendChild(bar);
+
+    const inputTextCity = document.createElement('input');
+    inputTextCity.classList.add('inputText')
+    bar.appendChild(inputTextCity)
+
+    const barContainer = document.createElement('div');
+    barContainer.classList.add('barContainer'); //do napisania w css bo tego nie ma
+    bar.appendChild(barContainer)
+
+    const searchButton = document.createElement('button');
+    searchButton.classList.add('button');  //w css jest button a nie.button
+    barContainer.appendChild(searchButton);
+
+    barContainer.addEventListener('click', () =>{
+        fetchWeatherData(inputTextCity.value)
+    })
+
+    const weatherImg = document.createElement('img');
+    weatherImg.classList.add('weatherImg'); //do napisania w css bo tego nie ma
+    app.appendChild(weatherImg);
+
+
+}
